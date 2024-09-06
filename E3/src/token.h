9541af07e6e2a9fe75e92d90755222b090025a5c
Ingrid <ingridcruz@infojr.com.br>
2024-09-06 20:22:24 -0300

    Update token.h

--- E3/src/token.h
+++ E3/src/token.h
@@ -1,13 +1,42 @@
 /* token.h */
 
 typedef enum {
-        ID = 256, 
-	NUM, 
-        KEY,
-        SYM,  
-        SYMC, 
-        CHAR,
-	STR,
-	ERROR
-} token_t; 
-
+    ID = 256,
+    NUM,
+    IF,    // Adicione palavras-chave como IF, ELSE, etc.
+    ELSE,
+    FOR,
+    WHILE,
+    RETURN,
+    PRINT,
+    CHAR,
+    STR,
+    LEQ,   // "less or equal"
+    GEQ,   // "greater or equal"
+    NEQ,   // "not equal"
+    EQ,    // "equal"
+    AND,   // "and"
+    OR,    // "or"
+    PLUS,  // "+"
+    MINUS, // "-"
+    MULT,  // "*"
+    DIV,   // "/"
+    MOD,   // "%"
+    NOT,   // "!"
+    LT,    // "<"
+    GT,    // ">"
+    ASSIGN, // "="
+    COLON, // ":"
+    SEMI,  // ";"
+    COMMA, // ","
+    LPAREN, // "("
+    RPAREN, // ")"
+    LBRACK, // "["
+    RBRACK, // "]"
+    LBRACE, // "{"
+    RBRACE, // "}"
+    BSLASH, // "\"
+    CHAR_LITERAL, // 'c'
+    STRING_LITERAL, // "string"
+    ERROR
+} token_t;
